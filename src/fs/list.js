89fcbaa38a9c3b21d8e2cb8be501c235de8a292e
Katsiaryna Karabeika <katerina.korobeiko@gmail.com>
2024-01-10 19:34:46 +0400

    feat: add decisions for fs tasks

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const srcDirPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const fileNames = await fs.readdir(srcDirPath);
+    fileNames.forEach((fileName) => console.log(fileName))
+  } catch (err) {
+    throw Error('FS operation failed');
+  }
 };
 
 await list();
