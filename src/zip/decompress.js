c2824ac7956cec01b51553840fbfc003917fb8d5
Katsiaryna Karabeika <katerina.korobeiko@gmail.com>
2024-01-11 00:31:02 +0400

    feat: add decisions for zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import fs, { write } from 'fs';
+import path from 'path';
+import { createUnzip } from 'zlib';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const destFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const srcFilePath = path.join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const readStream = fs.createReadStream(srcFilePath);
+    const writeStream = fs.createWriteStream(destFilePath);
+    const unzip = createUnzip()
+
+    readStream.pipe(unzip).pipe(writeStream);
+
+  } catch (err) {
+    throw Error('FS operation failed');
+  }
 };
 
 await decompress();
