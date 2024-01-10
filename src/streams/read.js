10eba07548d2e7a04baf0c4535430db0090d8b72
Katsiaryna Karabeika <katerina.korobeiko@gmail.com>
2024-01-10 22:57:37 +0400

    feat: add decisions for stream tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(process.stdout);
+  } catch (err) {
+    throw Error('FS operation failed');
+  }
 };
 
 await read();
